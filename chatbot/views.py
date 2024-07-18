645427fee0db755a670ffa3de28851cbe944e553
Junt Surani <junitsurani@gmail.com>
2024-07-18 15:35:12 +0530

    Added settings and interaction view

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ChatbotSettings, ChatbotInteraction
+from .serializers import ChatbotSettingsSerializer, ChatbotInteractionSerializer
 
 # Create your views here.
+
+class ChatbotSettingsViewSet(viewsets.ModelViewSet):
+    queryset = ChatbotSettings.objects.all()
+    serializer_class = ChatbotSettingsSerializer
+
+class ChatbotInteractionViewSet(viewsets.ModelViewSet):
+    queryset = ChatbotInteraction.objects.all()
+    serializer_class = ChatbotInteractionSerializer
