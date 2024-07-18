5b0e21ba77913050683873d734e556203353f46d
Junt Surani <junitsurani@gmail.com>
2024-07-18 15:33:57 +0530

    Created Model for settings and interaction

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ChatbotSettings(models.Model):
+    main_color = models.CharField(max_length=7, default="#6fd6ed")
+    send_message_color = models.CharField(max_length=7, default="#6fd6ed")
+    received_message_color = models.CharField(max_length=7, default="#ffffff")
+    background_color = models.CharField(max_length=7, default="#ffffff")
+    send_message_text_color = models.CharField(max_length=7, default="#ffffff")
+    received_message_text_color = models.CharField(max_length=7, default="#4b5563")
+
+class ChatbotInteraction(models.Model):
+    user_message = models.TextField()
+    bot_response = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
